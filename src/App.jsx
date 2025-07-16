8aed00e66bd6e2d3a46eaee6a2c91605c415e6e4
Alisson Gabriel <eng.alissongabriel@gmail.com>
2025-07-16 19:59:59 -0300

    chore: configurações iniciais e estruturação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-500 text-white p-6 text-center text-xl font-bold">
+      Tailwind está funcionando 🎉
+    </div>
+  );
+}
+
+export default App;
